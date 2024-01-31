de48b79a026929160036df27e4709c86d67a8ce4
MrSakine <msakine20@gmail.com>
2024-01-31 18:03:18 +0000

    [task-3] pycodestyle fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,13 +3,14 @@
 Creates and distributes an archive to your web servers
 """
 
+
 def deploy():
     """
     Full deployment based on do_pack & do_deploy (task 2)
     """
     do_pack = __import__("1-pack_web_static").do_pack
     do_deploy_util = __import__("2-do_deploy_web_static").do_deploy
-    
+
     path = do_pack()
     if path is None:
         return False
