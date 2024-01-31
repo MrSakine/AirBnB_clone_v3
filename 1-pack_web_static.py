197db80d222ade5a7551d7c10efac026b6f825ec
MrSakine <msakine20@gmail.com>
2024-01-31 16:46:21 +0000

    [task-1] do_pack return type

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,16 +11,20 @@ from fabric.api import local
 
 def do_pack():
     """Compress files from web_static directory"""
-    if not os.path.isdir("versions"):
-        os.makedirs("versions")
-    date = datetime.now()
-    file = "web_static_{0}{1}{2}{3}{4}{5}".format(
-        date.year,
-        date.month,
-        date.day,
-        date.hour,
-        date.minute,
-        date.second
-    )
-    file += ".tgz"
-    local("tar -cvzf versions/{} web_static".format(file))
+    try:
+        if not os.path.isdir("versions"):
+            os.makedirs("versions")
+        date = datetime.now()
+        file = "versions/web_static_{0}{1}{2}{3}{4}{5}".format(
+            date.year,
+            date.month,
+            date.day,
+            date.hour,
+            date.minute,
+            date.second
+        )
+        file += ".tgz"
+        local("tar -cvzf {} web_static".format(file))
+        return file
+    except Exception:
+        return None
