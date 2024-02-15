e33663410b433690c432737101b7f8b3c4d76db6
MrSakine <msakine20@gmail.com>
2024-02-15 12:17:42 +0000

    [task-3] python route added

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+This module is about python route
+"""
+
+from flask import Flask
+from werkzeug.routing import Rule
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text: str):
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python(text: str = "cool"):
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.endpoint("default_python")
+def default_python():
+    return "Python is cool"
+
+
+app.url_map.add(Rule("/python/", endpoint="default_python"))
+
+
+app.run()
