f36ac7170835747e3c9c4db89c6564a0b44c12a5
MrSakine <msakine20@gmail.com>
2024-02-15 18:13:18 +0000

    docstring added to the files

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,13 +11,16 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
+    """Display content from html files for the route"""
     all_states = storage.all(cls=State)
     return render_template("7-states_list", states=all_states)
 
 
 @app.teardown_appcontext()
-def close_storage():
+def close_storage(exception):
+    """Close sqlalchemy session after each request"""
     storage.close()
 
 
-app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
