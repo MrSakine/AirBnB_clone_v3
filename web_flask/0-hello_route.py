5481e83e53f6329a76cd9f165ead605ae991dec3
MrSakine <msakine20@gmail.com>
2024-02-15 08:31:59 +0000

    [web_framework] Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module is a minimal flask code
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello HBNB!"
+
+app.run()
