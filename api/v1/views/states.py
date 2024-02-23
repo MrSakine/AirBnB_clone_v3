a0cec75c0b0e4788a1665070ebfb08403a7ad4fd
MrSakine <msakine20@gmail.com>
2024-02-23 16:09:32 +0000

    [task-6] file import updated

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 """
-
+This module is about the state route of the API
 """
-
-from api.v1.views import app_views
 from flask import jsonify, request, abort, make_response
-from flasgger import Swagger, swag_from
 from models import storage, CLASSES
+from api.v1.views import app_views
 
 
 @app_views.route("/states", methods=["GET", "POST"])
-@swag_from(
-    "swagger_yaml/states/no_id_states.yml", methods=["GET", "POST"]
-)
-def no_id_states():
-    """ """
+def states():
+    """Get all states"""
     if request.method == "GET":
         return jsonify(
             [
@@ -39,21 +34,17 @@ def no_id_states():
 @app_views.route(
     "/states/<state_id>", methods=["GET", "DELETE", "PUT"]
 )
-@swag_from(
-    "swagger_yaml/states/id_sates.yml",
-    methods=["GET", "DELETE", "PUT"],
-)
-def id_states(state_id=None):
-    """ """
+def get_state(state_id=None):
+    """Get a specified state"""
     state = storage.get("State", state_id)
     if not state:
         abort(404)
     if request.method == "GET":
-        return jsonify(state.to_dict())
+        return make_response(jsonify(state.to_dict()), 200)
     if request.method == "DELETE":
         state.delete()
         storage.save()
-        return jsonify({}), 200
+        return make_response(jsonify({}), 200)
     if request.method == "PUT":
         if not request.get_json():
             return make_response(
@@ -63,4 +54,4 @@ def id_states(state_id=None):
             if key not in ["id", "created_at", "updated_at"]:
                 setattr(state, key, value)
         state.save()
-        return jsonify(state.to_dict())
+        return make_response(jsonify(state.to_dict()), 200)
